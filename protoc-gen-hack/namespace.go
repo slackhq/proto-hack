594428b47d01fb1958f2ca571b98beea352bc2f5
Jayant Tyagi <76139125+jtquip88@users.noreply.github.com>
2024-08-05 12:04:57 -0700

    Implement hack_namespace option for hack codegen (#31)
    Modify proto-hack codegen to support a custom namespace for generated
    hack code.
    A new option called `hack_namespace` can be added to .proto files as a
    compiler hint. Eg. `option (hack_namespace) = "fizzy\\bazzy";. When the
    hack_namespace option is set, the generated hack classes will be placed
    in the specified namespace.
    
    ### Change details:
    - The namespace at the top of generated class is changed to custom hack
    namespace if available.
    - If a file has `hack_namespace`, all references to its fields in other
    proto classes will use custom namespace prefix.
    When running the codegen, the first phase is to build a namespace tree
    for protobuf file. During this phase, a new attribute `cFqn` will also
    be populated. This attribute is the fully qualified name of the field
    prefixed with custom namespace. When generating output classes, `cFqn`
    will be used to refer to fields instead of `fqn`, if it is available.

--- protoc-gen-hack/namespace.go
+++ protoc-gen-hack/namespace.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	desc "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"regexp"
 	"strings"
+
+	desc "github.com/golang/protobuf/protoc-gen-go/descriptor"
 )
 
 // Names is a tree of structures and enums defined in a single namespace.
@@ -43,39 +45,59 @@ type Namespace struct {
 	Fqn      string
 	Names    *Names
 	Children map[string]*Namespace
+	// fully qualified name prefixed with custom namespace
+	cFqn string
 }
 
 func NewEmptyNamespace() *Namespace {
-	return newNamespace(nil, "")
+	customNs := ""
+	return newNamespace(nil, "", &customNs)
 }
 
-func newNamespace(parent *Namespace, myName string) *Namespace {
+// Builds a namespace tree with the given fully qualified name and custom fully qualified name.
+func newNamespace(parent *Namespace, myName string, customName *string) *Namespace {
 	fqn := myName + "."
+	cfqn := ""
+	if customName != nil {
+		cfqn = *customName + "."
+	}
 	if parent != nil {
 		fqn = parent.Fqn + fqn
+		cfqn = parent.cFqn + cfqn
 	}
 	return &Namespace{
 		parent:   parent,
 		Children: map[string]*Namespace{},
 		Names:    newNames(nil),
 		Fqn:      fqn,
+		cFqn:     cfqn,
 	}
 }
 
-func (n *Namespace) get(create bool, parts []string) *Namespace {
-	if len(parts) < 1 {
+// Returns the child Namespace corresponding to the given parts.
+// If create is true and the child does not exist, it creates the child Namespace.
+// If file has custom hack namespace, it also populates cFqn for every field in the tree.
+func (n *Namespace) get(create bool, parts []string, customNsParts []string) *Namespace {
+	if len(parts) == 0 {
 		return n
 	}
 	child := n.Children[parts[0]]
 	if child == nil {
 		if create {
-			child = newNamespace(n, parts[0])
+			var customName *string
+			if len(customNsParts) > 0 {
+				customName = &customNsParts[0]
+			}
+			child = newNamespace(n, parts[0], customName)
 			n.Children[parts[0]] = child
 		} else {
 			return nil
 		}
 	}
-	return child.get(create, parts[1:])
+	if len(customNsParts) == 0 {
+		return child.get(create, parts[1:], nil)
+	}
+	return child.get(create, parts[1:], customNsParts[1:])
 }
 
 // From any point in the namespace tree, decend to the root and then back up to
@@ -93,7 +115,7 @@ func (n *Namespace) FindFullyQualifiedNamespace(fqns string) *Namespace {
 		return n
 	}
 
-	found := n.get(false, strings.Split(strings.TrimPrefix(fqns, "."), "."))
+	found := n.get(false, strings.Split(strings.TrimPrefix(fqns, "."), "."), nil)
 	if found != nil {
 		return found
 	}
@@ -102,11 +124,19 @@ func (n *Namespace) FindFullyQualifiedNamespace(fqns string) *Namespace {
 
 func (n *Namespace) Parse(fdp *desc.FileDescriptorProto) {
 	pparts := []string{}
+	customNsparts := []string{}
 	if fdp.GetPackage() != "" {
 		pparts = strings.Split(fdp.GetPackage(), ".")
 	}
-
-	childns := n.get(true, pparts)
+	// If there is a custom namespace for the file, store the custom fully
+	// qualified name i.e. `cFqn` in the namespace tree while building it.
+	if cns := customHackNs(fdp); cns != "" {
+		if !isValidCustomHackNamespace(cns) {
+			panic(fmt.Errorf("Invalid custom hack namespace: %s", cns))
+		}
+		customNsparts = strings.Split(cns, "\\")
+	}
+	childns := n.get(true, pparts, customNsparts)
 
 	// Top level enums.
 	for _, edp := range fdp.EnumType {
@@ -150,29 +180,32 @@ func mustFullyQualified(fqn string) {
 // resolves it to a named entity and returns the proto name split at the
 // namespace boundary.
 // e.g. ".foo" "bar.baz"
+// Also returns the fully qualified name prefixed with custom namespace if
+// applicable.
 // and also returns the descriptor.
-func (n *Namespace) FindFullyQualifiedName(fqn string) (string, string, interface{}) {
+func (n *Namespace) FindFullyQualifiedName(fqn string) (string, string, interface{}, string) {
 	mustFullyQualified(fqn)
-	ns, name, i := n.find(fqn, true)
+	ns, name, i, cns := n.find(fqn, true)
 	if i == nil {
 		panic("couldn't resolve name: " + fqn)
 	}
 	ns = strings.TrimSuffix(ns, ".")
-	return ns, escapeReservedName(name), i
+	cns = strings.TrimSuffix(cns, ".")
+	return ns, escapeReservedName(name), i, cns
 }
 
-func (n *Namespace) find(fqn string, checkParent bool) (string, string, interface{}) {
+func (n *Namespace) find(fqn string, checkParent bool) (string, string, interface{}, string) {
 	if strings.HasPrefix(fqn, n.Fqn) {
 		// This name might be in our namespace
 		relative := strings.TrimPrefix(fqn, n.Fqn)
 		if name := n.Names.get(false, strings.Split(relative, ".")...); name != nil {
-			return n.Fqn, relative, name.descriptor
+			return n.Fqn, relative, name.descriptor, n.cFqn
 		}
 		// It may also be in a decendant namespace.
 		for _, childns := range n.Children {
-			rns, rname, i := childns.find(fqn, false)
+			rns, rname, i, cns := childns.find(fqn, false)
 			if rns != "" {
-				return rns, rname, i
+				return rns, rname, i, cns
 			}
 		}
 
@@ -182,5 +215,11 @@ func (n *Namespace) find(fqn string, checkParent bool) (string, string, interfac
 	if checkParent && n.parent != nil {
 		return n.parent.FindFullyQualifiedName(fqn)
 	}
-	return "", "", nil
+	return "", "", nil, ""
+}
+
+func isValidCustomHackNamespace(s string) bool {
+	pattern := `^(\w+\\)*\w+$`
+	re := regexp.MustCompile(pattern)
+	return re.MatchString(s)
 }
